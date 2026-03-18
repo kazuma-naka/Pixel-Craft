5cf65029ae9731252735713652e4e974f8389fa2
KazumaN1172 <80650553+kazuma-naka@users.noreply.github.com>
2026-03-18 14:26:37 -0400

    add PixelCraft.java and ARGB.java

---
+++ src/ARGB.java
@@ -0,0 +1,24 @@
+public class ARGB {
+    public int alpha;
+    public int red;
+    public int green;
+    public int blue;
+
+    public ARGB(int pixel) {
+        this.alpha = (pixel >> 24) & 0xff;
+        this.red = (pixel >> 16) & 0xff;
+        this.green = (pixel >> 8) & 0xff;
+        this.blue = pixel & 0xff;
+    }
+
+    public ARGB(int a, int r, int g, int b) {
+        this.alpha = a;
+        this.red = r;
+        this.green = g;
+        this.blue = b;
+    }
+
+    public int toInt() {
+        return (this.alpha << 24) | (this.red << 16) | (this.green << 8) | this.blue;
+    }
+}
